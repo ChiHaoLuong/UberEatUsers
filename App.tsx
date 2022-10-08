3c2cb32b57a63c3e1e0bb97d00889bd6ac2ff61a
ChiHaoLuong <lulubao1234@gmail.com>
2022-10-09 01:58:58 +0700

    initial

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView style={ }>
+
+    </SafeAreaView>
+  );
+};
+export default App;
