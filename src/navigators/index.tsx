85f0be4571195eb459945241c378e920b0898cf1
ChiHaoLuong <lulubao1234@gmail.com>
2022-10-22 16:54:43 +0700

    login

--- src/navigators/index.tsx
+++ src/navigators/index.tsx
@@ -1,20 +1,37 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { useEffect, useState } from 'react'
 import { Home, LandingScreen, PasswordInput, PhoneInputScreen } from '../screens'
 import { LoginParamList, RootParamList } from './type'
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
 const RootStack = createNativeStackNavigator<RootParamList>()
 
 const LoginStack = createNativeStackNavigator<LoginParamList>()
 
 export const RootStackScreen = () => {
-  // return (
-  //   <RootStack.Navigator >
-  //     <RootStack.Screen options={{
-  //       headerShown: false
-  //     }} name='home' component={Home} />
-  //   </RootStack.Navigator>
-  // )
+
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+  
+
+  useEffect(() => {
+    auth().onAuthStateChanged(userState => {
+      setUser(userState);
+
+      if (initializing) {
+        setInitializing(false);
+      }
+    });
+  }, []);
+
+  if (user) {
+    return (
+      <RootStack.Navigator screenOptions={{ headerShown: false }} >
+        <RootStack.Screen name='home' component={Home} />
+      </RootStack.Navigator>
+    )
+  }
 
   return (
     <LoginStack.Navigator screenOptions={{
